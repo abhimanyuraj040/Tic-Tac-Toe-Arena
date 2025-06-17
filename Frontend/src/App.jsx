331b95575a71932a4ee7848f10f7d71c4d954e23
Abhimanyu Kumar <67216514+abhimanyuraj040@users.noreply.github.com>
2025-06-17 17:45:19 +0530

    added new backend url in frontend

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -4,7 +4,7 @@ import React, { useState, useEffect } from "react";
 import { io } from "socket.io-client";
 
 // const socket = io("http://localhost:4000");
-const socket = io("https://tic-tac-toe-arena-xcgg.vercel.app/", {
+const socket = io("https://tic-tac-toe-arena.onrender.com", {
   withCredentials: true,
 });
 
